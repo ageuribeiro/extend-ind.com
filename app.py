080263eec95bcefa9bc9e75cffed030d1bf9d452
ageuribeiro <ageu87@gmail.com>
2023-07-15 15:42:52 -0300

    Changing structure to flask

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template, redirect, flash, request, send_file, send_from_directory, session, url_for
+from config import *
 import secrets
+
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
+
 
 # Define a rota principal para o usuario
 @app.route("/")
 def home():
-    return render_template("user/index.html", title='Showcase')
+    return render_template(
+        "index.html", 
+        title='Showcase',
+        endereco_loja=endereco_loja,
+        texto_destaque=texto_destaque,
+        whatsapp=whatsapp,
+        nome_loja=nome_loja,
+        whatsapp_link=whatsapp_link,
+        email=email,
+        telefone=telefone,
+        instagram_link=instagram_link)
 
 # Define a rota principal para o Administrador
 @app.route("/admin")
@@ -23,10 +36,50 @@ def admin_login():
 def admin_verify_account():
     return render_template("admin/verify_account.html", title='Verifying')
 
+# Define a rota para página Produtos
+@app.route('/produtos')
+def produtos():
+    return render_template('produtos/produtos.html')
 
-with app.test_request_context():
-    url = url_for('home')
-    print(url)
+# Define a rota para a página de Lista de Produtos
+@app.route('/lista_produtos')
+def lista_produtos():
+    return render_template('produtos/lista-produtos.html')
+
+# Define a rota para a página Sistemas
+@app.route('/sistemas')
+def sistemas():
+    return render_template('sistema')
+
+# Define a rota para a página de Categorias
+@app.route('/categorias')
+def categorias():
+    return render_template('estrutura/categorias.html')
+
+# Define a rota para a página de Promoções
+@app.route('/promotions')
+def promotions():
+    return render_template('estrutura/promocoes.html')
+
+# Define a rota para a página Contatos
+@app.route('/contatos')
+def contatos():
+    return render_template('estrutura/contatos.html')
+
+# Define a rota para a página Sobre
+@app.route('/sobre')
+def sobre():
+    return render_template('estrutura/sobre.html')
+
+# Define a rota par a página Carrinhos
+@app.route('/carrinho')
+def carrinho():
+    return render_template('estrutura/carrinho.html')
+
+# Define a rota para a página Blog
+@app.route('/blog')
+def blog():
+    return render_template('estrutura/blog.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=9000)
---
+++ config.py
@@ -0,0 +1,10 @@
+
+# VARIÁVEIS GLOBAIS
+email = 'bemchiquemodaelegante@gmail.com'
+whatsapp_link ='+5511977523331'
+telefone = '+55 (11) 97752-3331'
+whatsapp ='+55 (11) 97752-3331'
+instagram_link = 'https://www.instagram.com/bemchiques/'
+nome_loja = 'Boutique Bem Chique'
+texto_destaque = 'Produtos em Destaque!'
+endereco_loja = 'www.bemchique.com.br'
