a452d63469950334cd62329f8d1a9849a75064d8
ageuribeiro <ageu87@gmail.com>
2023-08-31 12:34:16 -0300

    novas atualizações

--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, abort, render_template, redirect, flash, request, send_file, send_from_directory, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
-from models import db, User
+from models import *
 from sqlalchemy import inspect
 import time
 import config
@@ -28,6 +28,16 @@ Session(app)
 @app.route("/")
 def home():
     categorias = ['Blusas', 'Camisetas', 'Moletons']
+    
+    username = None
+
+    if 'user_id' in session:
+        user_id = session['user_id']
+        user = User.query.get(user_id)
+        if user:
+            username = user.name
+
+    
     return render_template(
         "index.html", 
         categorias=categorias,
@@ -39,7 +49,8 @@ def home():
         whatsapp_link=config.whatsapp_link,
         email=config.email,
         telefone=config.telefone,
-        instagram_link=config.instagram_link
+        instagram_link=config.instagram_link,
+        username=username
         )
 
 # Define a rota para a página Sistemas
@@ -48,8 +59,8 @@ def sistemas():
    
     if request.method == 'POST':
           
-        email = request.form.get('email')
-        senha = request.form.get('senha')
+        email = request.form.get('login-email')
+        senha = request.form.get('login-senha')
 
         if not email or not senha : # Verifica se o existe email ou senha
             flash('Informe o email e a senha.','warning')
@@ -72,8 +83,19 @@ def sistemas():
     
     return render_template('sistema/index.html', title=title, email=email, texto_destaque=texto_destaque)
 
+
+# Define a rota para a página de Logout
+@app.route('/sistema/logout')
+def logout():
+    
+    if 'user_id' in session:
+        session.pop('user_id', None)
+    title = 'Thank You'
+    email = config.email
+    return redirect(url_for('home'))
+
 # Define a rota para a página de Cadastro
-@app.route('/sistema/register', methods=["POST", "GET"])
+@app.route('/sistema/register', methods=['GET', 'POST'])
 def register():
     email = config.email
     texto_destaque = config.texto_destaque
@@ -108,95 +130,129 @@ def register():
 
 
 # Define a rota para o painel de cliente
-@app.route('/sistema/painel/dashboard/<user>', methods=["GET","POST"])
+@app.route('/sistema/painel/dashboard/<user>', methods=['GET', 'POST'])
 def painel_client(user):
-    if 'user_id' not in session or session['user_id'] != int(user): # Verifica se há uma sessão ativa e se o usuário é o correto
-        print(f"session['user_id']: {session.get('user_id')}")
-        print(session)
-        print(f"user from URL: {user}")
+
+    if 'user_id' not in session or session['user_id'] !=int(user):
         abort(403) # Acesso negado
     
+    if 'user_id' in session:
+        user_id = session['user_id']
+        user = User.query.get(user_id)
+        if user:
+            username = user.name
+
     title = 'Cliente'
     email = config.email
     dados = f'Aqui é painel do {title}.'
-    return render_template('/sistema/painel-client/index.html', title=title, email=email, dados=dados)
+    return render_template('/sistema/painel-client/index.html', title=title, email=email, dados=dados, username=username)
 
 # Define a rota para o painel de administrador
-@app.route('/sistema/painel/manager/<admin>', methods=["GET","POST"])
+@app.route('/sistema/painel/manager/<admin>', methods=['GET', 'POST'])
 def painel_admin(admin):
+    username = None
     
     if 'user_id' not in session or session['user_id'] != int(admin):  # Verifica se há uma sessão ativa e se o usuário é o correto
-        print(f"session['user_id']: {session.get('user_id')}")
-        print(f"admin from URL: {admin}")
         abort(403)  # Acesso não autorizado
 
-    user = User.query.get(int(admin))
-    if user and user.profile != 'admin': # Obtem o objeto User do banco de dados pelo ID
-        abort(403) # Acesso não autorizado se o usuário não for um Administrador
+    if 'user_id' in session:
+        user_id =session['user_id']
+        user = User.query.get(user_id)
+        if user:
+            username = user.name
     
     title = 'Administrador'
     email = config.email
-    dados = f'Aqui é o Painel do {title}'
-    return render_template('/sistema/painel-admin/index.html', title=title, email=email, dados=dados)
+    return render_template('/sistema/painel-admin/index.html', title=title, email=email, username=username)
+    
 # Define a rota para página Produtos
-@app.route('/produtos')
+@app.route('/produtos', methods=['GET', 'POST'])
 def produtos():
     return render_template('produtos.html')
 
 # Define a rota para a página de Lista de Produtos
-@app.route('/lista_produtos')
+@app.route('/lista_produtos', methods=['GET', 'POST'])
 def lista_produtos():
     return render_template('lista-produtos.html')
 
+# Define a rota para a página de Gerenciamento de Categorias
+@app.route('/sistema/manager/categorias', methods=['GET', 'POST'])
+def manager_categorias():
+    return render_template('/sistema/categorias/index.html') 
+
+# Define a rota para o modal de novos produtos
+@app.route('/sistema/painel/manager/novo-produto')
+def novo_produto():
+    return render_template('/sistema/painel-admin/produto/novo_produto.html', conteudo_do_modal='Conteudo Dinâmico')
+
 # Define a rota para a página de Lista de Produtos Mais Vendidos
-@app.route('/mais_vendidos')
+@app.route('/mais_vendidos', methods=['GET', 'POST'])
 def mais_vendidos():
     return render_template('mais-vendidos.html')
 
 # Define a rota para a página de combos
-@app.route('/combos')
+@app.route('/combos', methods=['GET', 'POST'])
 def combos():
     return render_template('combos.html')
 
 # Define a rota para a página Novidades
-@app.route('/novidades')
+@app.route('/novidades', methods=['GET', 'POST'])
 def novidades():
-    return render_template('novidades.html')
-
+    return render_template('novidades.html', methods=['GET', 'POST'])
 
 # Define a rota para a página de Categorias
-@app.route('/categorias')
+@app.route('/categorias' , methods=['GET', 'POST'])
 def categorias():
-    return render_template('categorias.html')
+
+    pag = categorias()
+    menu = ["produtos","categorias","sub-categorias","combos","promocoes","clientes","vendas","backup"]
+    username = None
+
+    if 'user_id' in session:
+        user_id = session['user_id']
+        user = User.query.get(user_id)
+        if user:
+            username = user.name
+
+    return render_template('categorias.html', pag = pag, username = username, menu=menu)
 
 # Define a rota para a página de Promoções
-@app.route('/promotions')
+@app.route('/promotions', methods=['GET', 'POST'])
 def promotions():
     return render_template('promocoes.html')
 
 # Define a rota para a página Contatos
-@app.route('/contatos')
+@app.route('/contatos', methods=['GET', 'POST'])
 def contatos():
-    return render_template('contatos.html')
+    title='Contatos'
+    email = config.email
+    telefone =  config.telefone
+    return render_template('contatos.html', email=email, telefone=telefone, title=title)
 
 # Define a rota para a página Sobre
 @app.route('/sobre')
 def sobre():
-    return render_template('about.html')
+    title = 'Sobre'
+    email = config.email
+    telefone = config.telefone
+    return render_template('about.html', title=title, email=email, telefone=telefone)
 
 # Define a rota par a página Carrinhos
-@app.route('/carrinho')
+@app.route('/carrinho', methods=['GET', 'POST'])
 def carrinho():
     return render_template('carrinho.html')
 
 # Define a rota para a página Blog
-@app.route('/blog')
+@app.route('/blog', methods=['GET', 'POST'])
 def blog():
-    return render_template('blog.html')
+    title = 'Blog'
+    telefone = config.telefone
+    email = config.email
+    return render_template('blog.html', email=email, title=title, telefone=telefone)
 
 with app.app_context():
     inspector = inspect(db.engine)
-    if not inspector.has_table("user"):
+    if not inspector.has_table("User"):
         db.create_all()
 
     admin_username = config.ADMIN_USERNAME
